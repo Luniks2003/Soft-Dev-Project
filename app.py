697e344f7ba678cec49b9e535023968358b6a3fb
Loniel Kervin Alingasa <lonielkervin@gmail.com>
2024-03-31 22:20:33 +0800

    app

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request, redirect, url_for
+from distutils.log import debug 
+from fileinput import filename 
+from flask import *
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+UPLOAD_FOLDER = "uploads/"
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'html', 'pdf'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def index():
@@ -13,3 +21,10 @@ def about(name = None):
 @app.route("/convert")
 def convert():
     return render_template("convert.html")
+
+@app.route("/success", methods = ['GET', 'POST'])
+def success():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(app.config['UPLOAD_FOLDER'] + f.filename)
+    return render_template("success.html", name = f.filename)
