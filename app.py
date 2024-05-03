42d47072431ad3b98d852b4a17f2fd1345ac6fe9
Luniks2003 <lonielkervin@gmail.com>
2024-05-03 21:45:50 +0800

    added htr using 3 libraries

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ from fileinput import filename
 from flask import *
 import os
 from werkzeug.utils import secure_filename
+from PIL import Image
+import pytesseract
+import aspose.ocr as ocr
+import easyocr
+
+pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR\\tesseract.exe'
 
 app = Flask(__name__)
 UPLOAD_FOLDER = "uploads/"
@@ -15,7 +21,7 @@ def index():
     return render_template("index.html")
 
 @app.route("/about")
-def about(name = None):
+def about():
     return render_template("about.html")
 
 @app.route("/convert", methods = ['GET', 'POST'])
@@ -29,20 +35,18 @@ def convert():
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
         if file.filename == '':
-            flash('No selected file')
+            # flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('success', name=filename))
+            print("pytesseract: \n" + extract_text_from_image_pytesseract(UPLOAD_FOLDER+filename))
+            print("aspose: \n" + extract_text_from_image_aspose(UPLOAD_FOLDER+filename))
+            print("easyocr: \n")
+            extract_text_from_image_easyocr(UPLOAD_FOLDER+filename)
+            return redirect(request.url)
     return render_template("convert.html")
 
-# @app.route("/success", methods = ['GET', 'POST'])
-# def success():
-#     # if request.method == 'POST':
-#     #     file = request.files['file']
-#     #     file.save(app.config['UPLOAD_FOLDER'] + file.filename)
-#     return render_template("success.html", name = file.filename)
 @app.route("/success/<name>")
 def success(name = None):
     return render_template("success.html", name = name)
@@ -51,3 +55,28 @@ def success(name = None):
 def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+        
+def extract_text_from_image_pytesseract(image_path):
+    image = Image.open(image_path)
+    text = pytesseract.image_to_string(image)
+    return text
+
+def extract_text_from_image_aspose(image_path):
+    # Initialize an instance of Aspose.OCR API
+    api = ocr.AsposeOcr()
+    # Add image to the recognition batch
+    input = ocr.OcrInput(ocr.InputType.SINGLE_IMAGE)
+    input.add(image_path)
+    # Extract and show text
+    results = api.recognize_handwritten_text(input)
+    text = results[0].recognition_text
+    return text
+
+def extract_text_from_image_easyocr(image_path):
+    
+    reader = easyocr.Reader(['en']) # specify the language  
+    result = reader.readtext(image_path)
+
+    for (bbox, text, prob) in result:
+        print(f'Text: {text}, Probability: {prob}')
+        
